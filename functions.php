00a33bec4ad6e621dada89ee545d1a7f98efb0ad
heyitsuzair <uzairdevil354123@gmail.com>
2022-10-10 04:58:53 +0500

    Bootstrap Added

--- functions.php
+++ functions.php
@@ -9,6 +9,36 @@ function aquila_enqueue_scripts()
 {
     // wp_enqueue_style('stylesheet-1', get_template_directory_uri() . '/main.css', ['stylesheet-2']);
 
-    wp_enqueue_style('stylesheet-2', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
+    /**
+     * CSS Registration
+     * @package aquila
+     */
+
+    wp_register_style('stylesheet-2', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
+    wp_register_style('bootstrap_css', get_template_directory_uri() . '/assets/libraries/bootstrap/bootstrap.min.css', [], false, 'all');
+
+    /**
+     * CSS Enqueue
+     * @package aquila
+     */
+
+    wp_enqueue_style('stylesheet-2');
+    wp_enqueue_style('bootstrap_css');
+
+    /**
+     * JavaScript Registration
+     * @package aquila
+     */
+
+    wp_register_script('script-1', get_template_directory_uri() . '/assets/js/index.js', ['jquery'], filemtime(get_template_directory() . '/assets/js/index.js'), true);
+    wp_register_script('bootstrap_js', get_template_directory_uri() . '/assets/libraries/bootstrap/bootstrap.bundle.min.js', ['jquery'], false, true);
+
+    /**
+     * JavaScript Enqueue
+     * @package aquila
+     */
+
+    wp_enqueue_script('script-1');
+    wp_enqueue_script('bootstrap_js');
 }
 add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
--- header.php
+++ header.php
@@ -17,5 +17,9 @@
 </head>
 
 <body <?php body_class('my-class'); ?>>
-    <?php wp_body_open(); ?>
+    <?php
+    if (function_exists('wp_body_open')) {
+        wp_body_open();
+    }
+    ?>
     <header>Header</header>
