a6f09a70aef2a61e072f1400e6fcf5a57893e40d
heyitsuzair <uzairdevil354123@gmail.com>
2022-10-10 04:11:44 +0500

    wp_enqueue_style

--- functions.php
+++ functions.php
@@ -4,3 +4,11 @@
  * Theme Functions
  * @package aquila
  */
+
+function aquila_enqueue_scripts()
+{
+    // wp_enqueue_style('stylesheet-1', get_template_directory_uri() . '/main.css', ['stylesheet-2']);
+
+    wp_enqueue_style('stylesheet-2', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
+}
+add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
