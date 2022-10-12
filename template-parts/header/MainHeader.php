1cb0b40f5629004e81b99bf0fa874d70751ab554
heyitsuzair <uzairdevil354123@gmail.com>
2022-10-12 23:46:42 +0500

    Page Title

--- template-parts/header/MainHeader.php
+++ template-parts/header/MainHeader.php
@@ -21,7 +21,7 @@ $header_menus = wp_get_nav_menu_items($header_menu_id);
 ?>
 
 <nav class="navbar navbar-expand-lg bg-dark">
-    <div class="container-fluid">
+    <div class="container">
         <a class="navbar-brand text-light" href="#"><?php
                                                     if (function_exists('the_custom_logo')) {
                                                         the_custom_logo();
