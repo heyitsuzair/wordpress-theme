1ed0e1323775a7dd4534d08c1d10fa8946b656c1
heyitsuzair <uzairdevil354123@gmail.com>
2022-10-14 23:49:55 +0500

    single_post_title

--- template-parts/components/blog/entry-header.php
+++ template-parts/components/blog/entry-header.php
@@ -13,6 +13,8 @@ $post_id = get_the_ID();
 
 $hide_title = get_post_meta($post_id, '_hide_page_title', true);
 
+$heading_class = !empty($hide_title) && $hide_title === 'yes' ? 'hide' : "";
+
 $has_post_thumbnail = get_the_post_thumbnail();
 ?>
 
@@ -24,10 +26,18 @@ $has_post_thumbnail = get_the_post_thumbnail();
         <a href=<?php echo esc_url(get_permalink()); ?>>
             <?php
                 the_post_thumbnail('featured-thumbnail');
+
                 ?>
         </a>
     </div>
     <?php
     endif
+    ?>
+    <?php
+    if (is_single() || is_page()) {
+        printf('<h1 class="page-title text-dark %1$s">%2$s</h1>', esc_attr($heading_class), wp_kses_post(get_the_title()));
+    } else {
+        printf('<h2 class="entry-title mb-3"><a class="text-dark" href="%1$s">%2$s</a></h2>', esc_url(get_the_permalink()), wp_kses_post(get_the_title()));
+    }
     ?>
 </header>
