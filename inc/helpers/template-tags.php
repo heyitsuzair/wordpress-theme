89f9cc244b2630a222a46ac2ca2f24bc8c3bf725
heyitsuzair <uzairdevil354123@gmail.com>
2022-10-15 00:12:05 +0500

    get_the_date

--- inc/helpers/template-tags.php
+++ inc/helpers/template-tags.php
@@ -23,3 +23,25 @@
 // {
 //     echo get_the_post_thumbnail($post_id, $size, $additional_attr);
 // }
+
+function aquila_posted_on()
+{
+    $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+
+    // Post Is Modified 
+    if (get_the_time('U') !== get_the_modified_time('U')) {
+        $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+    }
+
+    $time_string = sprintf(
+        $time_string,
+        esc_attr(get_the_date(DATE_W3C)),
+        esc_attr(get_the_date()),
+        esc_attr(get_the_modified_date(DATE_W3C)),
+        esc_attr(get_the_modified_date()),
+    );
+
+    $posted_on = sprintf(esc_html_x('Posted On %s', 'post date', 'aquila'), '<a href="' . esc_url(get_the_permalink()) . '" rel="bookmark">' . $time_string . '</a>');
+
+    echo '<span class="posted-on text-secondary">' . $posted_on . '</span>';
+}
---
+++ template-parts/components/blog/entry-meta.php
@@ -0,0 +1,14 @@
+<?php
+
+/** Template for post entry header
+ * 
+ * 
+ * @package aquila
+ */
+?>
+
+<div class="entry-meta mb-3">
+    <?php
+    aquila_posted_on();
+    ?>
+</div>
