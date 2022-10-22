17585d6895723a48dcb0d8597bd9f27f438487ad
heyitsuzair <uzairdevil354123@gmail.com>
2022-10-22 05:25:47 +0500

    Svgr/Cli x Panel Settins

--- assets/js/gutenberg/blocks/headingwithicon/edit.js
+++ assets/js/gutenberg/blocks/headingwithicon/edit.js
@@ -1,14 +1,24 @@
-import { RichText, useBlockProps } from "@wordpress/block-editor";
+import {
+  RichText,
+  useBlockProps,
+  InspectorControls,
+} from "@wordpress/block-editor";
 import { __ } from "@wordpress/i18n";
+import { PanelBody, RadioControl } from "@wordpress/components";
+import { getIconComponent } from "./icons-map";
 
 const Edit = ({ className, attributes, setAttributes }) => {
-  const { content } = attributes;
+  const { content, option } = attributes;
 
   const blockProps = useBlockProps();
 
+  const HeadingIcon = getIconComponent(option);
+
   return (
     <div className="aquila-icon-heading">
-      <span className="aquila-icon-heading__heading"></span>
+      <span className="aquila-icon-heading__heading">
+        <HeadingIcon />
+      </span>
       <RichText
         {...blockProps}
         tagName="h4" // The tag here is the element output and editable in the admin
@@ -18,6 +28,20 @@ const Edit = ({ className, attributes, setAttributes }) => {
         onChange={(content) => setAttributes({ content })} // Store updated content as a block attribute
         placeholder={__("Heading...", "aquila")} // Display this text before any content has been added by the user
       />
+      <InspectorControls>
+        <PanelBody title={__("Block Settings", "aquila")}>
+          <RadioControl
+            label={__("Select Icon", "aquila")}
+            help={__("Control The Icon Selection", "aquila")}
+            selected={option}
+            options={[
+              { label: __("Dos", "aquila"), value: "dos" },
+              { label: __("Dont", "aquila"), value: "donts" },
+            ]}
+            onChange={(option) => setAttributes({ option })}
+          />
+        </PanelBody>
+      </InspectorControls>
     </div>
   );
 };
